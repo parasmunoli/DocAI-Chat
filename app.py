89aa2f798120dc6959ae547b8176ce9a871e51b5
Paras Munoli <parasmunoli@gmail.com>
2025-06-05 23:39:39 +0530

    Fixed Similarity Search

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct
 
 load_dotenv()
-client = QdrantClient(host=os.getenv("QDRANT_URL"), api_key=os.getenv("QDRANT_API_KEY"), port=6333)
-
+client = QdrantClient(url=os.getenv("QDRANT_URL"), api_key=os.getenv("QDRANT_API_KEY"))
 
 
 # --- Collection Management ---
